f8fcc55882f50fe1604a67645300f9efc9e43b7d
albritter <maxiritter@aol.com>
2016-06-10 09:35:54 +0200

    Cleaning up EventHandler

--- src/main/java/de/albritter/Utils/EventHandler.java
+++ src/main/java/de/albritter/Utils/EventHandler.java
@@ -1,4 +1,4 @@
-package de.albritter.Utils;
+package de.albritter.utils;
 
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -7,38 +7,30 @@ import java.util.Iterator;
  * Created by hhalbritter on 09.06.2016.
  */
 public class EventHandler {
-    private static ArrayList<UseRadioSelection> useRadioSelectionsAdd = new ArrayList<UseRadioSelection>();
-    private static ArrayList<UseRadioSelection> useRadioSelectionsUpdate = new ArrayList<UseRadioSelection>();
-    private static ArrayList<UseRadioSelection> useRadioSelectionsRemove = new ArrayList<UseRadioSelection>();
+    private static ArrayList<UseRadioSelection> useRadioSelection = new ArrayList<UseRadioSelection>();
+
 
     public static <T extends UseRadioSelection> void registerForRadioAdd(T obj) {
-        useRadioSelectionsUpdate.add(obj);
+        useRadioSelection.add(obj);
     }
 
-    public static <T extends UseRadioSelection> void registerForRadioUpdate(T obj) {
-        useRadioSelectionsUpdate.add(obj);
-    }
-
-    public static <T extends UseRadioSelection> void registerForRadioRemove(T obj) {
-        useRadioSelectionsUpdate.add(obj);
-    }
 
     public static void radioAdd() {
-        Iterator<UseRadioSelection> ite = useRadioSelectionsAdd.iterator();
+        Iterator<UseRadioSelection> ite = useRadioSelection.iterator();
         while (ite.hasNext()) {
             ite.next().selectAdd();
         }
     }
 
     public static void radioUpadte() {
-        Iterator<UseRadioSelection> ite = useRadioSelectionsUpdate.iterator();
+        Iterator<UseRadioSelection> ite = useRadioSelection.iterator();
         while (ite.hasNext()) {
             ite.next().selectUpdate();
         }
     }
 
     public static void radioRemove() {
-        Iterator<UseRadioSelection> ite = useRadioSelectionsRemove.iterator();
+        Iterator<UseRadioSelection> ite = useRadioSelection.iterator();
         while (ite.hasNext()) {
             ite.next().selectRemove();
         }
