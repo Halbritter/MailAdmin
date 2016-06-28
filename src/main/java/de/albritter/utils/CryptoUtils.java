9acd2136424cbc5d770098fbfa976a8748230494
albritter <maxiritter@aol.com>
2016-06-28 22:04:39 +0200

    Commit

--- src/main/java/de/albritter/utils/CryptoUtils.java
+++ src/main/java/de/albritter/utils/CryptoUtils.java
@@ -32,6 +32,6 @@ public class CryptoUtils {
         }
 
         //   return stringBuilder.toString();
-        return stringBuilder.toString() + "$";
+        return stringBuilder.toString();
     }
 }
